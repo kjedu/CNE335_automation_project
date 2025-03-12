c594053a09fe3e55a8573f20005b814a418f3e14
kjedu <kjvelazquezbecerra@student.rtc.edu>
2025-03-12 13:56:02 -0700

    main.py line 9: updated code to my name. line 15: added a server object named after the ec2_user, server ip is my instance public address. line 18: pings my ec2 server
    Server.py
    line 13: 5 pings to be sent, if all goes well, 5 pings should be displayed in the output.
    Line 14-15: if the 5 pings sent receive at least one reply than it’s a success
    Line 16-17: if the 5 pings sent don’t receive at least one reply than it’s not a success.

--- Server.py
+++ Server.py
@@ -1,3 +1,6 @@
+from http.client import responses
+import os
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
@@ -7,4 +10,8 @@ class Server:
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        response =  os.system(f"ping -c 5 {self.server_ip}") # -c 5 for macOS instead of -n 5
+        if response == 0:
+            return f"{self.server_ip} ping was successful" # at least one reply was received, good
+        else:
+            return f"{self.server_ip} ping was not successful" # no replies were received, 100% lost, bad
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
-# CNE 335 Fall
+# Kaylie Velazquez
+# CNE 335 Winter
+
+from Server import Server
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Kaylie")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
+    ec2_user = Server("54.218.9.30") #My ec2 public ip address from my instance
     # TODO - Call Ping method and print the results
+    ec2_user.ping()
